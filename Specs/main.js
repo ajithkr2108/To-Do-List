bce9e032bc00bece552601c684d4a9b53fac67d2
AJITH K R <ajithkr2108@gmail.com>
2023-03-01 15:36:33 +0530

    complete the filter method not overlapping and did some cs work

--- Specs/main.js
+++ Specs/main.js
@@ -4,6 +4,16 @@ let bgStyle = document.getElementById("categories-section");
 let styleBtns = document.getElementsByClassName("bg-clr-btn");
 let popUpForm = document.getElementById("pop-up-form");
 let reminderContainer =document.getElementById("reminder-container");
+let allBtn = document.getElementById("all-btn");
+let todayBtn = document.getElementById("today-btn");
+let tomorrowElements = document.getElementsByClassName("Tomorrow");
+let personalThings = document.getElementsByClassName("Personal");
+let workThings = document.getElementsByClassName("Work");
+let allLists =document.getElementsByClassName("list-container");
+let todayBtnElements =document.getElementsByClassName("Today");
+let cancelBtn = document.getElementById("cancel-btn");
+let addBtn = document.getElementById("add-btn");
+let submitBtn = document.getElementById("submit-btn");
 
 
 // Themes change
@@ -30,7 +40,7 @@ let counter = 0;
 
 // here we creating HTML structure for to do list dynamically , removing list from HTML structure and  ,adding and removing list data from array of OBJ
 
-popUpForm.addEventListener("submit",(e) => {
+submitBtn.addEventListener("click",(e) => {
 
     // form data into Obj
 
@@ -38,12 +48,14 @@ popUpForm.addEventListener("submit",(e) => {
     let fd = new FormData(popUpForm);
     let obj = Object.fromEntries(fd);
     arrObj.push(obj);
+    popUpForm.style.display="none";
+
 
     //  list tittle and remove btn
 
 
     let listContainer = document.createElement('div');
-    listContainer.classList.add('list-container');
+    listContainer.classList.add('list-container',obj.purpose,obj.day);
     let tittleBtnSection = document.createElement('div');
     tittleBtnSection.classList.add('tittle-btn-section');
     let removeBtnDiv = document.createElement('div');
@@ -133,15 +145,85 @@ popUpForm.addEventListener("submit",(e) => {
             return obj.id !==removeBtn.id;
         });
 
-        console.log(arrObj);
+        // console.log(arrObj);
 
     });
 
 });
 
+//filtering List
+
+allBtn.addEventListener("click",(e)=>{
+    
+    for(let allList of allLists){
+        allList.style.display="block";
+    }
+
+    todayBtn.style.color="#D8D5D7";
+    allBtn.style.color ="#272728";
+
+    personalBtn.style.fontWeight="lighter";
+    workBtn.style.fontWeight="lighter";
+    
+    
+})
+
+todayBtn.addEventListener("click", (e) =>{
+    for(let tomorrowElement of tomorrowElements){
+        tomorrowElement.style.display="none";
+    }
+    for(let todayBtnElement of todayBtnElements){
+        todayBtnElement.style.display="block";
+    }
+    todayBtn.style.color="#272728"
+    allBtn.style.color = "#D8D5D7"
+
+    personalBtn.style.fontWeight="lighter";
+    workBtn.style.fontWeight="lighter";
+
+    });
+
+personalBtn.addEventListener("click",(e)=>{
+        for(let workthing of workThings){
+            workthing.style.display="none";
+        };
+
+        for(let personalThing of personalThings){
+            personalThing.style.display="block";
+        };
+        personalBtn.style.fontWeight="bold";
+        workBtn.style.fontWeight="lighter";
+
+        todayBtn.style.color="#D8D5D7";
+        allBtn.style.color ="#272728";
+
+workBtn.addEventListener("click",(e)=>{
+        for(let personalThing of personalThings){
+            personalThing.style.display="none";
+        }
+        for(let workthing of workThings){
+            workthing.style.display="block";
+        };
+        personalBtn.style.fontWeight="lighter";
+        workBtn.style.fontWeight="bold";
+
+        todayBtn.style.color="#D8D5D7";
+        allBtn.style.color ="#272728";
+        
+    });
+
+    
+    
+})
 
 
+// add btn pop up 
+addBtn.addEventListener("click",(e)=>{
+    popUpForm.style.display="flex"
+})
+// cancel btn disapear 
 
-
-
+cancelBtn.addEventListener("click",(e)=>{
+    popUpForm.style.display="none";
+})
 
